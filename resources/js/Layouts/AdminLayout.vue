71680492466fee27445a447e7e822e234c554c84
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2024-05-25 17:46:05 +0100

    install flowbite , add admin layout with index page

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+import NavBar from "../Pages/Admin/Components/NavBar.vue";
+import SideBar from "../Pages/Admin/Components/SideBar.vue";
+import { Head, Link } from "@inertiajs/vue3";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+});
+</script>
+
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <!-- NavBar Start -->
+        <NavBar />
+        <!-- NavBar End -->
+
+        <!-- SideBar Start -->
+        <SideBar />
+        <!-- SideBar End -->
+        <main class="h-auto p-4 pt-20 md:ml-64">
+            <slot />
+        </main>
+    </div>
+</template>
---
+++ resources/js/Pages/Admin/index.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+import { Head, Link } from "@inertiajs/vue3";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+});
+</script>
+
+<template>
+    <Head title="Dashboard" />
+    <AdminLayout>
+        <div class="grid grid-cols-1 gap-4 mb-4 sm:grid-cols-2 lg:grid-cols-4">
+            <div
+                class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"
+            ></div>
+            <div
+                class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"
+            ></div>
+            <div
+                class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"
+            ></div>
+            <div
+                class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"
+            ></div>
+        </div>
+        <div
+            class="mb-4 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 h-96"
+        ></div>
+        <div class="grid grid-cols-2 gap-4 mb-4">
+            <div
+                class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"
+            ></div>
+            <div
+                class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"
+            ></div>
+            <div
+                class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"
+            ></div>
+            <div
+                class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"
+            ></div>
+        </div>
+        <div
+            class="mb-4 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 h-96"
+        ></div>
+        <div class="grid grid-cols-2 gap-4">
+            <div
+                class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"
+            ></div>
+            <div
+                class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"
+            ></div>
+            <div
+                class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"
+            ></div>
+            <div
+                class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"
+            ></div>
+        </div>
+    </AdminLayout>
+</template>
