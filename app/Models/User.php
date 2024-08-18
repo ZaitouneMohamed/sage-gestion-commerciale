922469ca206a20c27373297ba66859a29d3e3d47
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2024-08-18 12:12:59 +0100

    add spatier roles and permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, Billable;
+    use HasApiTokens, HasFactory, Notifiable, Billable,HasRoles;
 
     /**
      * The attributes that are mass assignable.
