cf7d7dae724fe3b95c5f49c9a52c455b5ce8e474
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2024-08-19 22:23:06 +0100

    Revert "add cashier package , with extend migration , add billabel in user model with taxes in appserviceprovider"
    This reverts commit 7e691e57736894b141ca6b4b8588049248cd11cf.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-use Laravel\Cashier\Cashier;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Cashier::calculateTaxes();
-        //
         View::composer(["pdf.facture"], function ($view) {
             $data = User::findOrFail(auth()->id())->load('Company');
             $view->with('data', $data);
